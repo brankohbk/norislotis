f97d7d89d3fd78b3ebef4bfa03b52a1b90661d00
brankohbk <brankoh@gmail.com>
2020-08-11 00:40:36 -0300

    Estilizado de plantilla general del sitio (Header y Footer)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-    // Fixes unnecessary scrolling in mobile
-    let vh, vw;
+// Fixes unnecessary scrolling in mobile
+let vh, vw;
 
-    function updateSize() {
-      vh = window.innerHeight * 0.01;
-      vw = window.innerWidth * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
+function updateSize() {
+  vh = window.innerHeight * 0.01;
+  vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-    }
-    window.addEventListener("resize", updateSize)
-    updateSize();
+}
+window.addEventListener("resize", updateSize)
+updateSize();
 
 
-    // 
+// SCROLL PROGRESS
+
+window.addEventListener("scroll", scrollProgress);
+
+function scrollProgress() {
+  const progressBar=document.getElementsByClassName("progress-bar")[0];
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
+  progressBar.style.width = scrolled + "%";
+  progressBar.setAttribute("aria-valuenow", ""+parseInt(scrolled)) ;
+}
+
+// CHANGE MENU ICON
+
